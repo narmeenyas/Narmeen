bb6da50b14b5d2e1ee45b32ba3a46d9e8ebe63d3
narmeen <narmeenyas15@gmail.com>
2021-11-26 08:43:26 +0200

    Narmeen

---
+++ app/src/main/java/com/example/narmeen/NotificationIntentService.java
@@ -0,0 +1,101 @@
+package com.example.narmeen;
+
+import android.app.IntentService;
+import android.app.Notification;
+import android.app.NotificationManager;
+import android.app.PendingIntent;
+import android.content.Intent;
+import android.content.Context;
+
+import androidx.core.app.NotificationManagerCompat;
+
+/**
+ * An {@link IntentService} subclass for handling asynchronous task requests in
+ * a service on a separate handler thread.
+ * <p>
+ * <p>
+ * TODO: Customize class - update intent actions, extra parameters and static
+ * helper methods.
+ */
+public class NotificationIntentService extends IntentService {
+
+    // TODO: Rename actions, choose action names that describe tasks that this
+    // IntentService can perform, e.g. ACTION_FETCH_NEW_ITEMS
+    private static final String ACTION_FOO = "com.example.narmeen.action.FOO";
+    private static final String ACTION_BAZ = "com.example.narmeen.action.BAZ";
+
+    // TODO: Rename parameters
+    private static final String EXTRA_PARAM1 = "com.example.narmeen.extra.PARAM1";
+    private static final String EXTRA_PARAM2 = "com.example.narmeen.extra.PARAM2";
+    private static final int NOTIFICATION_ID = 3;
+
+    public NotificationIntentService() {
+        super("NotificationIntentService");
+    }
+
+    /**
+     * Starts this service to perform action Foo with the given parameters. If
+     * the service is already performing a task this action will be queued.
+     *
+     * @see IntentService
+     */
+    // TODO: Customize helper method
+    public static void startActionFoo(Context context, String param1, String param2) {
+        Intent intent = new Intent(context, NotificationIntentService.class);
+        intent.setAction(ACTION_FOO);
+        intent.putExtra(EXTRA_PARAM1, param1);
+        intent.putExtra(EXTRA_PARAM2, param2);
+        context.startService(intent);
+    }
+
+    /**
+     * Starts this service to perform action Baz with the given parameters. If
+     * the service is already performing a task this action will be queued.
+     *
+     * @see IntentService
+     */
+    // TODO: Customize helper method
+    public static void startActionBaz(Context context, String param1, String param2) {
+        Intent intent = new Intent(context, NotificationIntentService.class);
+        intent.setAction(ACTION_BAZ);
+        intent.putExtra(EXTRA_PARAM1, param1);
+        intent.putExtra(EXTRA_PARAM2, param2);
+        context.startService(intent);
+    }
+
+    @Override
+    protected void onHandleIntent(Intent intent) {
+        Notification.Builder noBuilder=new Notification.Builder(this);
+        //todo need to change the notification title
+        noBuilder.setContentTitle("this is the notification title");
+        noBuilder.setContentText("this is the notification body");
+        noBuilder.setSmallIcon(R.drawable.ic_baseline_person_24);
+        //this is the notification icon
+        Intent noIntent1=new Intent(this,MainActivity.class);
+        //this intent will be pending until the user clicks on the notification and will activate the activity specified in the intent
+        PendingIntent pendingIntent=PendingIntent.getActivity(this,2,noIntent1,PendingIntent.FLAG_UPDATE_CURRENT);
+        noBuilder.setContentIntent(pendingIntent);
+
+        Notification notification= noBuilder.build();
+        NotificationManagerCompat managerCompat=NotificationManagerCompat.from(this);
+        managerCompat.notify(NOTIFICATION_ID,notification);
+    }
+
+    /**
+     * Handle action Foo in the provided background thread with the provided
+     * parameters.
+     */
+    private void handleActionFoo(String param1, String param2) {
+        // TODO: Handle action Foo
+        throw new UnsupportedOperationException("Not yet implemented");
+    }
+
+    /**
+     * Handle action Baz in the provided background thread with the provided
+     * parameters.
+     */
+    private void handleActionBaz(String param1, String param2) {
+        // TODO: Handle action Baz
+        throw new UnsupportedOperationException("Not yet implemented");
+    }
+}
