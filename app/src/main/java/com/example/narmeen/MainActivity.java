411aea0daeb46273d84677d920ae59fd74b132cf
narmeen <narmeenyas15@gmail.com>
2022-01-28 09:24:30 +0200

    Narmeen

--- app/src/main/java/com/example/narmeen/MainActivity.java
+++ app/src/main/java/com/example/narmeen/MainActivity.java
@@ -1,6 +1,8 @@
 package com.example.narmeen;
 
+import androidx.annotation.NonNull;
 import androidx.appcompat.app.AppCompatActivity;
+import androidx.fragment.app.Fragment;
 import androidx.fragment.app.FragmentContainerView;
 import androidx.navigation.NavController;
 import androidx.navigation.Navigation;
@@ -13,6 +15,7 @@ import android.content.DialogInterface;
 import android.content.Intent;
 import android.content.SharedPreferences;
 import android.os.Bundle;
+import android.view.MenuItem;
 import android.view.View;
 import android.widget.Button;
 import android.widget.EditText;
@@ -32,10 +35,6 @@ public class MainActivity extends AppCompatActivity  {
         setContentView(R.layout.activity_main);
         getSupportActionBar().hide();
 
-        //BottomNavigationView bottomNavigationView = findViewById(R.id.bottomNavigationView);
-        //NavController navController = Navigation.findNavController(this,  R.id.fragmentContainerView);
-        //NavigationUI.setupWithNavController(bottomNavigationView, navController);
-
 
         //this will start the service which in turn will start the music
         musicIntent= new Intent(this,MusicService.class);
@@ -47,5 +46,34 @@ public class MainActivity extends AppCompatActivity  {
         AlarmManager alarmManager = (AlarmManager) this.getSystemService(Context.ALARM_SERVICE);
         alarmManager.setRepeating(AlarmManager.RTC_WAKEUP,  System.currentTimeMillis(),
                 1000 * 60 * 1, pendingIntent);
+
+
+        BottomNavigationView bottomNavigationView = findViewById(R.id.bottomNavigationView);
+        bottomNavigationView.setOnNavigationItemSelectedListener(navListener);
+        getSupportFragmentManager().beginTransaction().replace(R.id.fragment_container,new ProfileFragment()).commit();
     }
+  private BottomNavigationView.OnNavigationItemSelectedListener navListener =
+          new BottomNavigationView.OnNavigationItemSelectedListener() {
+              @Override
+              public boolean onNavigationItemSelected(@NonNull MenuItem item) {
+                  Fragment selectedFragment = null;
+                  switch (item.getItemId()){
+                      case R.id.topicsFragment:
+                          selectedFragment=new TopicsFragment();
+                          break;
+                      case R.id.collectionFragment:
+                          selectedFragment=new CollectionFragment();
+                          break;
+                      case R.id.profileFragment:
+                          selectedFragment=new ProfileFragment();
+                          break;
+                      case R.id.premiumFragment:
+                          selectedFragment=new PremiumFragment();
+                          break;
+                  }
+                  getSupportFragmentManager().beginTransaction().replace(R.id.fragment_container,selectedFragment).commit();
+                  return true;
+
+              }
+          };
 }
