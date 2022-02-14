ad84536e3e5d4188b5046c1fd0ce0056b79732b3
narmeen <narmeenyas15@gmail.com>
2022-02-14 12:59:40 +0200

    Narmeen

--- app/src/main/java/com/example/narmeen/MainActivity.java
+++ app/src/main/java/com/example/narmeen/MainActivity.java
@@ -1,31 +1,21 @@
 package com.example.narmeen;
 
-import androidx.annotation.NonNull;
-import androidx.appcompat.app.AppCompatActivity;
-import androidx.fragment.app.Fragment;
-import androidx.fragment.app.FragmentContainerView;
-import androidx.navigation.NavController;
-import androidx.navigation.Navigation;
-import androidx.navigation.ui.NavigationUI;
-
 import android.app.AlarmManager;
 import android.app.PendingIntent;
 import android.content.Context;
-import android.content.DialogInterface;
 import android.content.Intent;
-import android.content.SharedPreferences;
 import android.os.Bundle;
 import android.view.MenuItem;
-import android.view.View;
-import android.widget.Button;
-import android.widget.EditText;
+
+import androidx.annotation.NonNull;
+import androidx.appcompat.app.AppCompatActivity;
+import androidx.fragment.app.Fragment;
 
 import com.google.android.material.bottomnavigation.BottomNavigationView;
 
 public class MainActivity extends AppCompatActivity  {
 
     private static final int NOTIFICATION_REMINDER_NIGHT = 1;//this is for the broadcast receiver
-
 
     private Intent musicIntent;
 
@@ -47,10 +37,10 @@ public class MainActivity extends AppCompatActivity  {
         alarmManager.setRepeating(AlarmManager.RTC_WAKEUP,  System.currentTimeMillis(),
                 1000 * 60 * 1, pendingIntent);
 
-
-        BottomNavigationView bottomNavigationView = findViewById(R.id.bottomNavigationView);
+  // navigation bar code below (needs fixing)
+        BottomNavigationView bottomNavigationView = findViewById(R.id.bottom_navigation);
         bottomNavigationView.setOnNavigationItemSelectedListener(navListener);
-        getSupportFragmentManager().beginTransaction().replace(R.id.fragment_container,new ProfileFragment()).commit();
+        getSupportFragmentManager().beginTransaction().replace(R.id.fragment_container,new CoursesFragment()).commit();
     }
   private BottomNavigationView.OnNavigationItemSelectedListener navListener =
           new BottomNavigationView.OnNavigationItemSelectedListener() {
@@ -58,17 +48,17 @@ public class MainActivity extends AppCompatActivity  {
               public boolean onNavigationItemSelected(@NonNull MenuItem item) {
                   Fragment selectedFragment = null;
                   switch (item.getItemId()){
-                      case R.id.topicsFragment:
-                          selectedFragment=new TopicsFragment();
+                      case R.id.nav_courses:
+                          selectedFragment=new CoursesFragment();
                           break;
-                      case R.id.collectionFragment:
-                          selectedFragment=new CollectionFragment();
+                      case R.id.nav_extra:
+                          selectedFragment=new ExtraFragment();
                           break;
-                      case R.id.profileFragment:
-                          selectedFragment=new ProfileFragment();
+                      case R.id.nav_profiles:
+                          selectedFragment=new ProfilesFragment();
                           break;
-                      case R.id.premiumFragment:
-                          selectedFragment=new PremiumFragment();
+                      case R.id.nav_premiu:
+                          selectedFragment=new PremiuFragment();
                           break;
                   }
                   getSupportFragmentManager().beginTransaction().replace(R.id.fragment_container,selectedFragment).commit();
@@ -76,4 +66,6 @@ public class MainActivity extends AppCompatActivity  {
 
               }
           };
+    //see the quiz video resource 13:11 - this makes the option chosen highlighted which could be useful in
+    //multiple answer questions
 }
